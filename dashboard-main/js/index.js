b71b7cdf651106f4513bf2cfd1a301af6f13905d
Tirth-Moradiya <132430774+Tirth-Moradiya@users.noreply.github.com>
2023-05-03 11:58:47 +0530

    Add files via upload

---
+++ dashboard-main/js/index.js
@@ -0,0 +1,19 @@
+const closeBtn = document.querySelector("#close-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const sideMenu = document.querySelector("aside");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle("dark-theme-varaibles");
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle("active");
+})
